ce74c0af0e3fd49dc1beee7336a2f9f1a7a9a436
saveychauhan <sav.ey@live.co.uk>
2026-05-02 01:07:35 +0530

    Auto-commit on mutation 2026-05-02 01:07:35

--- core/settings.py
+++ core/settings.py
@@ -74,7 +74,10 @@ ASGI_APPLICATION = 'core.asgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
     },
 }
 
