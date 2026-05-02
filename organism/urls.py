a4e28118fe6da07c95d7232ffcf43c8459ea9ecb
saveychauhan <sav.ey@live.co.uk>
2026-05-02 10:15:06 +0530

    Auto-commit on mutation 2026-05-02 10:15:06

--- organism/urls.py
+++ organism/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('chat/', views.chat_view, name='chat_post'),
     path('reset/', views.reset_chat, name='reset'),
     path('models/', views.get_models, name='get_models'),
+    path('engines/', views.get_media_engines, name='get_media_engines'),
     path('set_model/', views.set_model, name='set_model'),
     path('tasks/', views.get_tasks, name='get_tasks'),
 ]
